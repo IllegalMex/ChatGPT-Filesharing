a23fd4a168681764d93daf8dc0d80e6cbaf08e68
IllegalMex <99988436+IllegalMex@users.noreply.github.com>
2023-03-19 21:38:45 +0100

    Update: Nutzererfahrung verbessert
    Neue Funktionen:
    - reCAPTCHA für das Uploadform integriert, um die Sicherheit zu erhöhen
    - zulässige Dateitypen können definiert werden
    - maximale Uploadgröße kann definiert werden
    - maximale Anzahl an gleichzeitig hochzuladenen Dateien kann definiert werden
    - Nach dem Upload werden Dateilinks in der Langversion benutzerfreundlich zum schnellen Kopieren angezeigt.
    - Benutzer können ihre selbst hochgeladenen Dateien wieder löschen, sofern der Cookie noch aktiv ist.
    
    Fix:
    - Fehlermeldungen werden nun benutzerfreundlicher dargestellt
    
    bekannte Bugs:
    - Beim Upload großer Dateien erzeugt das reCAPTCHA Plugin eine Fehlermeldung, der Uploadprozess wird jedoch nicht gestoppt

--- index.php
+++ index.php
@@ -7,47 +7,99 @@ $links = [];
 // Wenn das Formular zum Hochladen von Dateien gesendet wurde
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploadFile']))
 {
-    $uploadDir = 'data/';
+    // Überprüfung des reCaptcha-Status
+    $url = 'https://www.google.com/recaptcha/api/siteverify';
+    $data = array(
+        'secret' => 'SECRET_KEY_EINGEBEN',
+        'response' => $_POST['g-recaptcha-response']
+    );
+    $options = array(
+        'http' => array (
+            'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    $response = json_decode($result);
 
-    // Schleife über alle hochgeladenen Dateien
-    foreach ($_FILES['uploadFile']['name'] as $i => $fileName)
+    if ($response->success)
     {
-        // Datei in den Upload-Ordner speichern
-        $uploadFile = $uploadDir . basename($fileName);
-        move_uploaded_file($_FILES['uploadFile']['tmp_name'][$i], $uploadFile);
+        $uploadDir = 'data/';
 
-        // Eindeutigen Link erstellen
-        $link = uniqid();
+        // Check if number of uploaded files exceed 20
+if (count($_FILES['uploadFile']['name']) > 20) {
+    $uploadError = "Es k&uuml;nnen maximal 20 Dateien gleichzeitig hochgeladen werden.";
+    $_SESSION['uploadError'] = $uploadError;
+    header("Location: index.php");
+    exit; // Stop file upload
+}
+        // Schleife über alle hochgeladenen Dateien
+    foreach ($_FILES['uploadFile']['name'] as $i => $fileName) {
+        // Prüfen, ob der Dateityp und die Dateigröße zulässig sind
+        $allowedTypes = ['txt', 'zip', 'jpg', 'rar'];
+        $maxSize = 1 * 1024 * 1024; // 100 MB in Bytes
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $fileSize = $_FILES['uploadFile']['size'][$i];
+        if (!in_array($fileType, $allowedTypes)) {
+            $uploadError = 'Nur Dateien mit den Endungen .txt, .jpg, .zip oder .rar sind erlaubt.';
+	$_SESSION['uploadError'] = $uploadError;
+	header("Location: index.php");
+            exit;
+        } else if ($fileSize > $maxSize) {
+            $uploadError = 'Die maximale Dateigr&ouml;&szlig;e von 100 MB wurde &uuml;berschritten.';
+	$_SESSION['uploadError'] = $uploadError;
+	header("Location: index.php");
+            exit;
+        } else {
+            // Datei in den Upload-Ordner speichern
+            $uploadFile = $uploadDir . basename($fileName);
+            move_uploaded_file($_FILES['uploadFile']['tmp_name'][$i], $uploadFile);
 
-        // Prüfen ob der Link bereits in der Datenbank vorhanden ist
-        $stmt = $conn->prepare("SELECT * FROM files WHERE link = ?");
-        $stmt->bind_param("s", $link);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        while ($result->num_rows > 0)
-        {
+            // Eindeutigen Link erstellen
             $link = uniqid();
+
+            // Prüfen ob der Link bereits in der Datenbank vorhanden ist
+            $stmt = $conn->prepare("SELECT * FROM files WHERE link = ?");
             $stmt->bind_param("s", $link);
             $stmt->execute();
             $result = $stmt->get_result();
+
+            while ($result->num_rows > 0)
+            {
+                $link = uniqid();
+                $stmt->bind_param("s", $link);
+                $stmt->execute();
+                $result = $stmt->get_result();
+            }
+
+            // Dateiinformationen in der Datenbank speichern
+            $stmt = $conn->prepare("INSERT INTO files (filename, link, upload_date) VALUES (?, ?, NOW())");
+            $stmt->bind_param("ss", $fileName, $link);
+            $stmt->execute();
+
+            // Cookie mit dem Dateilink setzen, wenn es nicht schon existiert
+            if (!isset($_COOKIE["file_$link"]))
+            {
+                // Gültigkeit Cookie für 365 Tage
+                setcookie("file_$link", $fileName, time() + 3600 * 24 * 30, "/");
+            }
+            $links[] = $link;
+		$uploadDone = [];
+		$_SESSION['uploadDone'] = $uploadDone;
         }
+    }
 
-        // Dateiinformationen in der Datenbank speichern
-        $stmt = $conn->prepare("INSERT INTO files (filename, link, upload_date) VALUES (?, ?, NOW())");
-        $stmt->bind_param("ss", $fileName, $link);
-        $stmt->execute();
 
-        // Cookie mit dem Dateilink setzen, wenn es nicht schon existiert
-        if (!isset($_COOKIE["file_$link"]))
-        {
-            setcookie("file_$link", $fileName, time() + 3600 * 24 * 30, "/");
-        }
 
-        $links[] = $link;
+}
+    else
+    {
+        // Wenn reCaptcha-Überprüfung fehlschlägt, wird eine Fehlermeldung ausgegeben
+        $uploadError = 'Ung&uuml;ltiges Recaptcha';
     }
 }
-
 // Array zum Speichern der Links aus dem Cookie
 $cookieLinks = [];
 
@@ -81,19 +133,50 @@ if (!empty($cookieLinks))
     $links = array_merge($links, $cookieLinks);
 }
 
+
+
 ?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Meine Dateifreigabe-Seite</title>
+    <title>ChatGPT Filesharing</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
     <h1>ChatGPT Filesharing</h1>
+
+<! Fehlermeldung anzeigen>
+<?php if(isset($_SESSION['uploadError'])): ?>
+<form>
+    <p style="color: red; font-weight: bold;"><?php echo $_SESSION['uploadError']; ?></p>
+</form>
+<?php unset($_SESSION['uploadError']); ?>
+<?php endif; ?>
+
+<!Generierten Downloadlink anzeigen>
+<?php if(isset($_SESSION['uploadDone'])): ?>
+<form>
+<?php // Wenn mindestens eine Datei hochgeladen wurde, werden die Links angezeigt
+if (count($links) > 0) {
+    echo "Neue Links zum Herunterladen:<br>";
+    foreach ($links as $link) {
+        if (!in_array($link, $cookieLinks)) {
+            echo "https://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/download.php?link=" . $link . "<br>";
+        }
+    }
+}
+?>
+    
+</form>
+<?php unset($_SESSION['uploadDone']); ?>
+<?php endif; ?>
+
     <form action="index.php" method="post" enctype="multipart/form-data">
         <label for="uploadFile">Dateien ausw&auml;hlen:</label>
         <input type="file" name="uploadFile[]" id="uploadFile" multiple>
+      <div class="g-recaptcha" data-sitekey="SITEKEY_EINGEBEN"></div>
         <input type="submit" value="Hochladen">
     </form>
     <?php if (!empty($links)): ?>
@@ -105,6 +188,7 @@ if (!empty($cookieLinks))
                 <th>Link</th>
                 <th>Dateiname</th>
                 <th>Uploaddatum</th>
+                <th>Aktionen</th>
             </tr>
         </thead>
         <tbody>
@@ -123,6 +207,7 @@ if (!empty($cookieLinks))
                     </td>
                     <td><?php echo $file['filename'] ?></td>
                     <td><?php echo $file['upload_date'] ?></td>
+                    <td><a href="deleteself.php?link=<?php echo $link ?>">L&ouml;schen</a></td>
                 </tr>
             <?php
     endforeach; ?>
